d37807067562572f6011029c7f3a5d6ab9651022
Pankaj Choudhary <choudhary.pankaj@tftus.com>
2019-01-31 09:50:24 +0530

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require("path");
+var srcPath = path.resolve(__dirname, "src");
+var distPath = path.resolve(__dirname, "dist");
+
+var config = {
+  entry: [path.resolve(srcPath, "app.js")],
+  output: {
+    path: distPath,
+    filename: "bundle.js",
+    publicPath: "/"
+  },
+  devServer: {
+    inline: true,
+    port: 4500
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        include: /src/,
+        use: "babel-loader",
+      }
+    ]
+  }
+};
+
+module.exports = config;
