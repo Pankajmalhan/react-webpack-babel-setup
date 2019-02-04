3e68eea058d6d2f4968892392fc890fb1befc2af
TFTUS\choudhary.pankaj <choudhary.pankaj@tftus.com>
2019-02-04 15:54:05 +0530

    2000 changes

--- src/app.js
+++ src/app.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./components/Main";
 
-ReactDOM.render(<Main name="1800" />, document.getElementById("app"));
+ReactDOM.render(<Main name="2000" />, document.getElementById("app"));
