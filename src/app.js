1dd1929cbd9a5d4cf3065089b411653a91e51efa
TFTUS\choudhary.pankaj <choudhary.pankaj@tftus.com>
2019-02-05 13:38:15 +0530

    last commit

--- src/app.js
+++ src/app.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./components/Main";
 
-console.log("pankaj");
+console.log("pankaj 420");
 ReactDOM.render(<Main name="1200" />, document.getElementById("app"));
