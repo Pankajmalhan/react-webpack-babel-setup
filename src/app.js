b19c06d7b0ce26c49b5e2538be045266c0424c44
TFTUS\choudhary.pankaj <choudhary.pankaj@tftus.com>
2019-02-05 13:38:56 +0530

    120

--- src/app.js
+++ src/app.js
@@ -2,5 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./components/Main";
 
-console.log("pankaj 420");
 ReactDOM.render(<Main name="1200" />, document.getElementById("app"));
